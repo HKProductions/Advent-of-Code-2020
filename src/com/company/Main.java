9e8372475a5b34f7a92ba34815c50bed729f8980
HKProductions <hkproductions9@gmail.com>
2020-12-01 01:43:54 -0700

    Minor update to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Day1 d1 = new Day1("/Users/hk/Desktop/Advent-of-Code-2020/data/day1Part1.txt"); //<- Day 1
+        Day1 d1 = new Day1("/Advent-of-Code-2020/data/day1Part1.txt"); //<- Day 1
         d1.sum2020Multiple(2020, 2);
         d1.sum2020Multiple(2020, 3);
 
